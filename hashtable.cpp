01938cfb2df480ca2eddd5e4c45310cf3bd05c7a
sihala <ssihala@gmail.com>
2022-07-21 21:48:36 -0400

    Create Hashtable and implement basic functions

---
+++ hashtable.cpp
@@ -0,0 +1,80 @@
+//
+// Created by sihala on 7/20/22.
+//
+
+#include "hashtable.h"
+
+Hashtable::Hashtable() {
+    numKeys=0;
+    numBuckets=1572869;
+    hashtable.resize(numBuckets);
+}
+
+int Hashtable::hashFunction(int key) const {
+    return key%numBuckets;
+}
+
+void Hashtable::expandTable() {
+    std::vector<Book> newTable;
+    newTable.resize(numBuckets*2);
+    numBuckets*=2;
+
+    for(Book element : hashtable)
+        if(element.id != -1)
+            insertKey(newTable, element.id, element);
+
+    hashtable = newTable;
+}
+
+void Hashtable::insertKey(int key, Book& value) {
+    int index= hashFunction(key);
+    numKeys++;
+    if(hashtable[index].id==-1)
+        hashtable[index]=value;
+    else{
+        //COLLISION RESOLUTION-QUADRATIC PROBING
+        for(int i=0; i<numBuckets; i++){
+            if(hashtable[(index + i*i)%numBuckets].id== -1){
+                hashtable[(index + i*i)%numBuckets]=value;
+                break;
+            }
+        }
+    }
+
+    if((float)numKeys/(float)numBuckets > MAX_LOADFACTOR)
+        expandTable();
+}
+
+void Hashtable::insertKey(std::vector<Book> &table, int key, Book &value) const {
+    int index= hashFunction(key);
+    if(table[index].id==-1)
+        table[index]=value;
+    else{
+        //COLLISION RESOLUTION-QUADRATIC PROBING
+        for(int i=0; i<numBuckets; i++){
+            if(table[(index + i*i)%numBuckets].id== -1){
+                table[(index + i*i)%numBuckets]=value;
+                break;
+            }
+        }
+    }
+}
+
+Book Hashtable::searchKey(int key) {
+    int index = hashFunction(key);
+    if(hashtable[index].id == key)
+        return hashtable[index];
+    else{
+        for(int i=0; i<numBuckets; i++){
+            if(hashtable[(index + i*i)%numBuckets].id== key){
+                return hashtable[(index + i*i)%numBuckets];
+            }
+        }
+    }
+
+    Book notFound;
+    return notFound;
+}
+
+
+
---
+++ hashtable.h
@@ -0,0 +1,27 @@
+//
+// Created by sihala on 7/20/22.
+//
+
+#pragma once
+#include<vector>
+#include <vector>
+#include "book.h"
+
+class Hashtable {
+    private:
+        int numKeys;
+        //CHOOSE GOOD PRIME NUMBER- referenced from https://www.planetmath.org/goodhashtableprimes, hashtable will be doubled to accomodate all 2.3 million books.
+        int numBuckets;
+        float const MAX_LOADFACTOR=0.75;
+        std::vector<Book> hashtable;
+    public:
+        Hashtable();
+        int hashFunction(int key) const;
+        void expandTable();
+        void insertKey(std::vector<Book>& table, int key, Book& value) const;
+        void insertKey(int key, Book& value);
+        Book searchKey(int key);
+
+};
+
+

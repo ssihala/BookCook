17b943d22c920de3b3bf771ccc64f0bd3639d881
sihala <ssihala@gmail.com>
2022-08-01 16:59:43 -0400

    Remove description and url attributes

--- src/book.h
+++ src/book.h
@@ -12,24 +12,18 @@ struct Book{
         title="";
         isbn="";
         avgRating=-1;
-        description="";
-        url="";
         publisher="";
         numPages=-1;
         publicationYear=-1;
-        imageURL="";
     }
     int id;
     std::string title;
     std::string isbn;
     double avgRating;
     std::vector<int> similarBooks;
-    std::string description;
-    std::string url;
     std::vector<std::string> authors;
     std::vector<std::string> genres;
     std::string publisher;
     int numPages;
     int publicationYear;
-    std::string imageURL;
 };

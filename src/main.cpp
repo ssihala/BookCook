7a2509292a5cf0f453745e008f0779281b168ca8
JasonLiang19 <67469516+JasonLiang19@users.noreply.github.com>
2022-08-01 21:27:18 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include "book.h"
+#include "Graph.h"
+#include "hashtable.h"
+#include "SpeedTests.h"
+#include "Input.h"
+
+#include <iostream>
+
+int main(){
+
+    
+
+    std::cin.clear();
+    int input = 1;
+
+    while (input != 0){
+        std::cout << "1: Enter Book" << std::endl;
+        std::cout << "2: Graph vs Hashtable Speed Comparison" << std::endl;
+        std::cout << "0: Exit" << std::endl; 
+        std::cin >> input; 
+
+        std::string bookName;
+        Graph graph;
+        Hashtable hash;
+        std::unordered_map<int, std::string> idMap;
+        int numElements;
+        switch (input){
+            case 1:
+                std::cin.ignore();
+                getline(std::cin, bookName);
+                loadHash(hash,idMap);
+                if (hash.searchKey(bookName).id == -1) // if book isn't in table
+                    std::cout << "Book not found" << std::endl << std::endl;
+                else {
+                    for (int i = 0; i < hash.searchKey(bookName).similarBooks.size(), i++){
+                        std::cout << idMap.at(hash.searchKey(bookName).similarBooks.at(i)) << std::endl;
+                    }
+                    std::cout << std::endl; // spacing
+                }
+                break; 
+            case 2:
+                std::cout << "Enter Number of Elements to be Tested: " << std::endl;
+                std::cin >> numElements; 
+                insertionTest(hash, graph, numElements);
+                searchTest(hash, graph);
+                break;
+            case 0: 
+                break; 
+            default:
+                std::cout << "Invalid Input" << std::endl; 
+        }
+    }
+
+    return 0;
+}
+
+
+
